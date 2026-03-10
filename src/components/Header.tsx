a3c0aeb7c81e8370bab02453981e5b85dad7cd03
David <davidomonge8@gmail.com>
2026-03-10 19:26:59 +0300

    Just the Header start

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import '../componentStyles/Header.css'
+
+const Header = () => {
+    return(
+        <div className = "Header">
+                
+        </div>
+    )
+}
+
+export default Header
