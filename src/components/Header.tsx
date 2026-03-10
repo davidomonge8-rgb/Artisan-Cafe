f94841a92a0db785509aa37ecae064b425e3e50f
David <davidomonge8@gmail.com>
2026-03-10 19:46:42 +0300

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
 import '../componentStyles/Header.css'
 
+import Magnet from '../ReactBits/Magnet.jsx'
+
+
+
 const Header = () => {
     return(
         <div className = "Header">
-                
+                <div className="brandName">
+
+                    <img src="searchicon.svg" alt="search" />
+
+                    <Magnet padding={50} disabled={false} magnetStrength={10}>
+                        
+                        <h1><i>Artisan Cafe</i></h1>
+                    </Magnet>
+
+                </div>
         </div>
     )
 }
