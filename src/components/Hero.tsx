90f93b3e21f7e0a27d626998838135ff7be6ebe5
David <davidomonge8@gmail.com>
2026-03-11 16:04:47 +0300

    Hero button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,18 @@
 import '../componentStyles/Hero.css';
+import add from '../assets/plus-small.png'
 
 const Hero = () => {
     return(
-        <div className='Hero'>
+        <div className='hero'>
             
             <div className="hero-text">
                 <h1>Summer <i style={{ color: 'rgb(152, 109, 62)' }}>Aisne</i> 2024</h1>
                 <p>A curated selection of seasonal delights, meticulously crafted to celebrate the vibrant flavors of high summer in our Parisian-inspired atelier.</p>
             </div>
             
-            
+            <div className="New-entry">
+                <button><img src={add} alt="New Entry" /> CREATE NEW ENTRY</button>
+            </div>
 
 
         </div>
