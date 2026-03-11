bb782eb2a9ca01ed83b8449fe8b040d81eac0d56
David <davidomonge8@gmail.com>
2026-03-11 19:40:34 +0300

    Commit but take a look at the how the pictures have been implemented

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,45 @@
 import '../componentStyles/Menu.css'
+import matcha from '../assets/matcha.jpg'
+import pizza from '../assets/pizza.jpg'
+import eggOnToast from '../assets/eggOnToast.jpg'
 
 const Menu = () =>{
     return(
-        <>
-        Menu says hello
-        </>
+        <div className='menu'>
+            <div className="menu-items">
+
+                <div className="matcha">
+                    <h1 className= "menu-image"></h1>
+                    <div className="description">
+                        <h2>Matcha</h2>
+                        <p>$1.6</p>
+                    </div>
+                </div>
+
+                <div className="matcha">
+                    <h1 className= "menu-image"></h1>
+                    <div className="description">
+                        <h2>Matcha</h2>
+                        <p>$1.6</p>
+                    </div>
+                </div>
+
+                <div className="matcha">
+                    <h1 className= "menu-image"></h1>
+                    <div className="description">
+                        <h2>Matcha</h2>
+                        <p>$1.6</p>
+                    </div>
+                </div>
+
+                
+
+                
+
+
+                
+            </div>
+        </div>
     )
 }
 
